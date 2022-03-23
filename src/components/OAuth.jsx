961e71a89eb0b0c7b574a16869ba933fb78e5bfe
asalas <67822688+andrewjsalas@users.noreply.github.com>
2022-03-22 17:39:01 -0700

    Created Google Auth Functionality
    Created the ability for users to be able to sign in or sign up using their Gmail account.

---
+++ src/components/OAuth.jsx
@@ -0,0 +1,50 @@
+import { useLocation, useNavigate } from "react-router-dom"
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore"
+import { db } from "../firebase.config"
+import { toast } from "react-toastify"
+import googleIcon from '../assets/svg/googleIcon.svg'
+
+function OAuth() {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  // Allows users to sign or sign-up using their gmail account
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user
+
+      // Checks to see if user already exits, and if not, adds user.
+      // Get reference to the document
+      const docRef = doc(db, 'users', user.uid)
+      const docSnap = await getDoc(docRef)
+
+      // Checks to see if user exits in database
+      if (!docSnap.exists()) {
+        await setDoc(doc(db, 'users', user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timeStamp: serverTimestamp()
+        })
+      }
+      // Once signed in or signed-up, navigate to 'home'
+      navigate('/')
+    } catch (error) {
+      toast.error('Could not authorize with Google')
+    }
+  }
+
+  return (
+    <div className="socialLogin">
+      <p>Sign {location.pathname === '/sign-up' ? 'up' : 'in'} with </p>
+      <button className="socialIconDiv" onClick={onGoogleClick}>
+        <img className="socialIconImg" src={googleIcon} alt='google' />
+      </button>
+    </div>
+  )
+}
+
+export default OAuth
